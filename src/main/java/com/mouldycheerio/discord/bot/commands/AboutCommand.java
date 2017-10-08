386d108c204f3c7fbcb1654edbcbeba8f7aba47f
AdiiCloud <32550376+AdiiCloud@users.noreply.github.com>
2017-10-08 13:23:55 +0200

    Create AboutCommand.java

---
+++ src/main/java/com/mouldycheerio/discord/bot/commands/AboutCommand.java
@@ -0,0 +1,18 @@
+package com.mouldycheerio.discord.bot.commands;
+
+import com.mouldycheerio.discord.bot.MyBot;
+
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IMessage;
+
+public class AboutCommand extends BotCommand {
+    public AboutCommand() {
+        setName("about");
+        setDescription(new CommandDescription("Info About The Bot And Owners Of The Bot."));
+    }
+
+    public void onCommand(MyBot orangepeel, IDiscordClient client, IMessage commandMessage, String[] args) {
+        commandMessage.getChannel().sendMessage("**Adii-Bot**. This is a bot, created by **Adii#8807**, and **davidovski#8648**. Contact them for help or further info.");
+
+    }
+}
